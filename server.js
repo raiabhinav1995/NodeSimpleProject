ea52b1d57cf550da2b2d0601affbd6172c2028fc
Abhinav <raiabhinav1995@gmail.com>
2020-08-29 20:05:26 +0530

    Added dynamic rendering based on parameter passed

--- server.js
+++ server.js
@@ -5,17 +5,25 @@ const fs=require('fs');
 let port=8080;
 http.createServer(function(req, res)
 {
-    fs.readFile('helloWorld.html', (err, result)=>
+    const q = url.parse(req.url, true);
+    let fileName='.'+q.pathname;
+    fs.readFile(fileName, (err, result)=>
     {
+        if(err)
+        {
+            res.writeHead(404, {'Content-Type': 'text,html'});
+            return res.end('404 not found');
+        }
+        
         res.writeHead(200, {'Content-Type': 'text/html'});
     // res.write(`You have request for ${req.url} endpoint`);
     res.write(result);
-    var data=url.parse(req.url, true).query;
-    res.write(`The date today is ${data.date} and the year is ${data.year}\n\n`);
-    res.write(`\nServer is running on port ${port}\n\n`);
-    res.write('Hello World\n\n'+ 'Date is  ' + date.currentDate());
-    res.end();
-    })
+    var data=q.query;
+    return res.end();
+    // res.write(`The date today is ${data.date} and the year is ${data.year}\n\n`);
+    // res.write(`\nServer is running on port ${port}\n\n`);
+    // res.write('Hello World\n\n'+ 'Date is  ' + date.currentDate());
+    // res.end();
+    });
     
-}
-    ).listen(port);
+}).listen(port);
