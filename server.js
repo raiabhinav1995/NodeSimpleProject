6fe5eec53fb7ce0a17867e57b8b3b9e22a30c160
Abhinav <raiabhinav1995@gmail.com>
2020-08-30 05:19:30 +0530

    Removing node modules

--- server.js
+++ server.js
@@ -5,10 +5,37 @@ const fs=require('fs');
 const events=require('events');
 const formidable=require('formidable');
 const credentials=require('./credentials');
+const sql=require('mysql');
+const { log } = require('console');
+
+
+
+
+
+
+//----------------------------------------------------------
 let transporter=credentials.transporter;
 let mailOptions=credentials.mailOptions;
 let port=8080;
 let eventEmitter=new events.EventEmitter();
+
+var con = sql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "secret",// Ubuntu user forever :),
+    database: "mydb"
+  });
+  console.log(global);
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+  });
 http.createServer(function(req, res)
 {
     const q = url.parse(req.url, true);
@@ -31,7 +58,8 @@ http.createServer(function(req, res)
     //return res.end();
     res.write(`\nServer is running on port ${port}\n\n`);
     res.write('Hello World\n\n'+ 'Date is  ' + date.currentDate());
-    // sendEmail('test', 'test');
+    // for(let i=0;i<100;i++);
+    // sendEmail('test', 'Do not worry this is just a test m');
     return res.end();
     });
     
